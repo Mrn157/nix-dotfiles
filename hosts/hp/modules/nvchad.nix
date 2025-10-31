24fc0c6d057afc5971227253c9ac0a0d47790aa1
Mrn157 <gamerzzz1510@gmail.com>
2025-11-01 12:55:44 +1300

    Doing something

--- hosts/hp/modules/nvchad.nix
+++ hosts/hp/modules/nvchad.nix
@@ -8,22 +8,26 @@
     chadrcConfig = ''
     local M = {}
 
-M.base46 = {
-	theme = "chadracula-evondev",
+    local M = {
+      base46 = {
+        theme = "chadracula-evondev",
+        -- hl_override = {
+        --   Comment = { italic = true },
+        --   ["@comment"] = { italic = true },
+        -- },
+      },
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
-}
+      nvdash = {
+        load_on_startup = true,
+      },
 
-M.nvdash = { load_on_startup = true }
-M.ui = {
-      transparency = true,
-      tabufline = {
-         lazyload = false
-     },
-}
+      ui = {
+        transparency = true,
+        tabufline = {
+          lazyload = false,
+        },
+      },
+    }
 
 return M
     '';
