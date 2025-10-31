9be4fef9d341d6dce58697b11261668718707e11
Mrn157 <gamerzzz1510@gmail.com>
2025-10-31 21:12:53 +1300

    Fix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Fully reproducible NixOS + Home Manager dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
