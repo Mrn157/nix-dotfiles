82d7392526235650dfb400ccc4f5cd33cf30154e
Mrn157 <gamerzzz1510@gmail.com>
2025-10-31 23:15:53 +1300

    Doing something

--- hosts/hp/modules/rofi.nix
+++ hosts/hp/modules/rofi.nix
@@ -4,5 +4,5 @@
   home.packages = [ pkgs.rofi ];
 
   # Place your config alongside this file, e.g. ./fastfetch.jsonc
-  xdg.configFile."rofi/launchers/type-2/style-1.rasi".source = ./rofi/launchers/type-2/style-2.rasi;
+  xdg.configFile."rofi/launchers/type-2/style-1.rasi".source = ./rofi/launchers/type-2/style-1.rasi;
 }
