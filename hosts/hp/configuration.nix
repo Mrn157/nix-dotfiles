89f5793d43c7e8f582aa84adb7fb6d1f94e014fe
Mrn157 <gamerzzz1510@gmail.com>
2025-11-01 14:48:48 +1300

    Added package 'gdu'

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -66,7 +66,7 @@ in
     neovim wget foot nemo nwg-look git fastfetch floorp rofi-wayland
     udisks2 udiskie ffmpeg_6-full waybar pulsemixer swaybg vulkan-tools
     brightnessctl grim slurp rose-pine-cursor wl-clipboard viewnior 
-    rose-pine-hyprcursor fzf gcc zsh blueman btop
+    rose-pine-hyprcursor fzf gcc zsh blueman btop gdu
 # For NUR packages add pkgs. before it 
     pkgs.nur.repos.ataraxiasjel.waydroid-script
     ninja meson plocate gnumake cage-xtmapper mpv 
