797e10d5936079a9e669b83928de2c7b8331a33a
Mrn157 <gamerzzz1510@gmail.com>
2025-11-02 20:15:11 +1300

    Removed the hp-dwl folder

--- hosts/hp-dwl/modules/fastfetch.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  home.packages = [ pkgs.fastfetch ];
-
-  # Place your config alongside this file, e.g. ./fastfetch.jsonc
-  xdg.configFile."fastfetch/config.jsonc".source = ./config.jsonc;
-}
--- hosts/hp-dwl/modules/rofi.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  home.packages = [ pkgs.rofi ];
-
-  # Place your config alongside this file, e.g. ./fastfetch.jsonc
-  xdg.configFile."rofi/launchers/type-2/style-1.rasi".source = ./rofi/launchers/type-2/style-1.rasi;
-}
