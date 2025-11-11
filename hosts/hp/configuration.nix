45dfee8878d86b376092d43be924c93972722d60
Mrn157 <gamerzzz1510@gmail.com>
2025-11-11 19:18:08 +1300

    Changed plymouth theme

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -120,7 +120,10 @@ in
   boot = {
     plymouth = {
       enable = true;
-      theme = "bgrt";
+      theme = "nixos-bgrt";
+      themePackages = with pkgs; [
+      nixos-bgrt-plymouth
+      ];
     };
 
     # Enable "Silent boot"
