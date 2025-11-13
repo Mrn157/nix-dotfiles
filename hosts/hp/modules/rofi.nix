faa8ed604f3c533b2a1412805b7a01896ec47bf8
Mrn157 <gamerzzz1510@gmail.com>
2025-11-13 17:21:26 +1300

    Fixed rofi after deleting files

--- hosts/hp/modules/rofi.nix
+++ hosts/hp/modules/rofi.nix
@@ -4,5 +4,5 @@
   home.packages = [ pkgs.rofi ];
 
   # Place your config alongside this file, e.g. ./fastfetch.jsonc
-  xdg.configFile."rofi/style-1.rasi".source = ./rofi/style-1.rasi;
+  xdg.configFile."./rofi".source = ./rofi;
 }
