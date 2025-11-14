398dd6db4f317f2c0215122ec5d3a57bd267fb79
Mrn157 <gamerzzz1510@gmail.com>
2025-11-15 12:35:35 +1300

    Added gospeed

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -74,7 +74,7 @@ in
   environment.systemPackages = with pkgs; [
     neovim wget foot nemo nwg-look git fastfetch appimage-run floorp rofi-wayland unzip cargo
     udisks2 udiskie ffmpeg_6-full waybar pulsemixer swaybg vulkan-tools kdePackages.kdenlive
-    brightnessctl grim slurp rose-pine-cursor wl-clipboard viewnior riseup-vpn openshot-qt
+    brightnessctl grim slurp rose-pine-cursor wl-clipboard viewnior riseup-vpn openshot-qt gopeed
     rose-pine-hyprcursor fzf gcc zsh blueman gdu protonup-ng palemoon-bin protontricks cloudflare-warp
 # For NUR packages add pkgs. before it 
     pkgs.nur.repos.ataraxiasjel.waydroid-script
