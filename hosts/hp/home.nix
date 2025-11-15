ac8d820d721eb577f8ee3b466c6f23d0f132121e
Mrn157 <gamerzzz1510@gmail.com>
2025-11-15 19:06:35 +1300

    Add comment to Change GTK Theme source...

--- hosts/hp/home.nix
+++ hosts/hp/home.nix
@@ -13,6 +13,9 @@ let
 
     # To fix the error > cp: missing destination file operand after ''
     sourceRoot = "Rosepine-Purple-Dark-Release/Rosepine-Purple-Dark";
+    # Or use cp -r Rosepine-Purple-Dark/* $out/share/themes/Rosepine-Purple-Dark in installPhase
+    # This works because after unpackPhase, Nix will automatically pick the top directory of the ARCHIVE
+    # (Which is Rosepine-Purple-Dark-Release)
 
     installPhase = ''
       mkdir -p $out/share/themes/Rosepine-Purple-Dark
