599bcbc1d2ba78bce8d37abdefecc13573f3b45b
Mrn157 <gamerzzz1510@gmail.com>
2025-11-21 18:43:25 +1300

    Changed niri's applauncher to anyrun (rofi segfaults)

--- hosts/hp/home.nix
+++ hosts/hp/home.nix
@@ -147,6 +147,26 @@ in
     '';
   };
 
+  programs.anyrun = {
+    enable = true;
+    config = {
+      x = { fraction = 0.5; };
+      y = { fraction = 0.3; };
+      width = { fraction = 0.3; };
+      hideIcons = false;
+      ignoreExclusiveZones = false;
+      layer = "overlay";
+      hidePluginInfo = false;
+      closeOnClick = false;
+      showResultsImmediately = false;
+      maxEntries = null;
+
+      plugins = [
+        "${pkgs.anyrun}/lib/libapplications.so"
+      ];
+    };
+  };
+
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
