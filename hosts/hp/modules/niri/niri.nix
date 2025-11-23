1f32660e41394fc4e40ba50c3b00e1a46c32da5a
Mrn157 <gamerzzz1510@gmail.com>
2025-11-23 18:10:33 +1300

    Moved XDG autostart edits from home.nix to niri.nix and added useful comment

--- hosts/hp/modules/niri/niri.nix
+++ hosts/hp/modules/niri/niri.nix
@@ -4,4 +4,26 @@
 
   # Place your config alongside this file, e.g. ./fastfetch.jsonc
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
+
+  # Disable XDG autostarted apps
+  # Quickly check if theres any unwanted programs XDG is running
+  /* ls /run/current-system/sw/etc/xdg/autostart/ 
+     ls ~/.config/autostart/
+  */
+  xdg.configFile."autostart/blueman.desktop".text = ''
+    [Desktop Entry]
+    Hidden=true
+  '';
+  xdg.configFile."autostart/gnome-keyring-pkcs11.desktop".text = ''
+    [Desktop Entry]
+    Hidden=true
+  '';
+  xdg.configFile."autostart/gnome-keyring-secrets.desktop".text = ''
+    [Desktop Entry]
+    Hidden=true
+  '';
+  xdg.configFile."autostart/gnome-keyring-ssh.desktop".text = ''
+    [Desktop Entry]
+    Hidden=true
+  '';
 }
