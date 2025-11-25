dd8634f3fda07d6269209552740300be3ee422de
Mrn157 <gamerzzz1510@gmail.com>
2025-11-25 16:52:15 +1300

    Configuration.nix changes, to make it easier to read + cagextmapper installer moved a .nix file

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -1,76 +1,14 @@
 { config, lib, pkgs, nur, pkgs-unstable, ... }:
 
-let
-  # Local derivation example
-  cage-xtmapper = pkgs.stdenv.mkDerivation {
-    pname = "cage-xtmapper";
-    version = "1.0";
-    src = ./pkgs/cage-xtmapper/cage-xtmapper-v0.2.0.tar;
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = with pkgs; [
-      wayland libdrm libxkbcommon pixman mesa vulkan-loader systemd seatd
-      xorg.libxcb xorg.xcbutilrenderutil xorg.xcbutil libGL
-    ];
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp local/bin/cage_xtmapper $out/bin/
-      cp local/bin/cage_xtmapper.sh $out/bin/
-      chmod +x $out/bin/*
-    '';
-  };
-in
 {
   imports = [
     ./hardware-configuration.nix
   ];
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Home Manager options (module is imported via flake.nix)
-  home-manager.useUserPackages = true;
-  home-manager.useGlobalPkgs = true;
-  boot.loader.systemd-boot.enable = true;
-  systemd.extraConfig = ''
-    DefaultTimeoutStopSec=10s
-    LogLevel=emerg
-  '';
-  boot.loader.systemd-boot.consoleMode = "max";
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "hp";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Pacific/Auckland";
-  services.timesyncd.enable = true;
-
-
-  fileSystems."/run/media/mrn1/data" = {
-    device = "/dev/disk/by-uuid/06EE19DCEE19C539";
-    fsType = "ntfs-3g";
-    options = [ "rw" "uid=1000" "gid=100" "umask=0022" ];
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "steam"
-    "steam-original"
-    "steam-unwrapped"
-    "steam-run"
-  ];
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  users.users.mrn1 = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    packages = with pkgs; [ tree ];
-  };
-
+  ################
+  ### PACKAGES ###
+  ###############
+  
   environment.systemPackages = 
   (with pkgs; [
     neovim wget foot nemo-with-extensions nwg-look git fastfetch appimage-run floorp rofi-wayland unzip cargo
@@ -78,12 +16,11 @@ in
     brightnessctl grim slurp rose-pine-cursor wl-clipboard viewnior riseup-vpn openshot-qt 
     rose-pine-hyprcursor fzf gcc zsh blueman gdu protonup-ng palemoon-bin protontricks
     mission-center gfn-electron xwayland-satellite wev wgcf wireguard-tools
-    nix-init zed-editor cemu
-
-
+    nix-init zed-editor cemu 
+    (import ./pkgs/cage-xtmapper/cage-xtmapper.nix { pkgs = pkgs; })
 # For NUR packages add pkgs. before it 
     pkgs.nur.repos.ataraxiasjel.waydroid-script
-    ninja meson plocate gnumake cage-xtmapper mpv tmux dwl p7zip unrar lutris neovide steam-run xorg.libSM
+    ninja meson plocate gnumake /* cage-xtmapper */ mpv tmux dwl p7zip unrar lutris neovide steam-run xorg.libSM
 
   ])
   
@@ -93,15 +30,75 @@ in
   waydroid-helper
   ]);
 
-  
+  fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-unwrapped"
+    "steam-run"
+  ];
   
  nixpkgs.config.permittedInsecurePackages = [
    "electron-35.7.5"
  ];
 
-  # Auto Login Change mrn1 to your username
+  nixpkgs.overlays = [
+    (final: prev: {
+      dwl = prev.dwl.overrideAttrs (old: rec {
+        version = "ab4cb6e28365cf8754d6d3bdd293c05abfc27e26";
+        src = builtins.fetchGit {
+          url = "https://codeberg.org/dwl/dwl";
+          rev = version;
+        };
+        buildInputs = old.buildInputs ++ [ 
+        prev.wlroots_0_19
+        prev.fcft
+        prev.libdrm
+        ];
+        nativeBuildInputs = old.nativeBuildInputs ++ [ prev.pkg-config ];
 
-  services.getty.autologinUser = "mrn1";
+        patches = (old.patches or []) ++ [
+        (prev.fetchpatch {
+          excludes = [ "config.def.h" ];
+          url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/bar/bar.patch";
+          sha256 = "sha256-EZNorxpiNLa4q70vz4uUAiH6x36N/F5oPQ0iJp3m5Nw=";
+         })
+
+        (prev.fetchpatch {
+          excludes = [ "config.def.h" ];
+          url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/autostart/autostart.patch";
+          sha256 = "sha256-f+41++4R22HYtAwHbaRk05TMKCC8mgspwBuNvnmbQfU=";
+         })
+        (prev.fetchpatch {
+          excludes = [ "config.def.h" ];
+          url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/cursortheme/cursortheme.patch";
+          sha256 = "sha256-E544m6ca2lYbjYxyThr3GQEhDqh2SDjryLV/g4X8Rt4=";
+         })
+        ];
+        postPatch = ''
+        cp ${./modules/dwl/config.def.h} config.def.h
+        '';
+      });
+    })
+  ];
+
+
+  ################
+  ### SERVICES ###
+  ################
+  services = {
+    # Auto Login Change mrn1 to your username
+    getty.autologinUser = "mrn1";
+    udisks2.enable = true;
+    flatpak.enable = true;
+    pipewire = {
+    enable = true;
+    pulse.enable = true;
+    };
+  };
+
+
 
   ################
   ### PROGRAMS ###
@@ -161,54 +158,31 @@ in
     hyprland.enable = true;
 
   };
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      dwl = prev.dwl.overrideAttrs (old: rec {
-        version = "ab4cb6e28365cf8754d6d3bdd293c05abfc27e26";
-        src = builtins.fetchGit {
-          url = "https://codeberg.org/dwl/dwl";
-          rev = version;
-        };
-        buildInputs = old.buildInputs ++ [ 
-        prev.wlroots_0_19
-        prev.fcft
-        prev.libdrm
-        ];
-        nativeBuildInputs = old.nativeBuildInputs ++ [ prev.pkg-config ];
-
-        patches = (old.patches or []) ++ [
-        (prev.fetchpatch {
-          excludes = [ "config.def.h" ];
-          url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/bar/bar.patch";
-          sha256 = "sha256-EZNorxpiNLa4q70vz4uUAiH6x36N/F5oPQ0iJp3m5Nw=";
-         })
-
-        (prev.fetchpatch {
-          excludes = [ "config.def.h" ];
-          url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/autostart/autostart.patch";
-          sha256 = "sha256-f+41++4R22HYtAwHbaRk05TMKCC8mgspwBuNvnmbQfU=";
-         })
-        (prev.fetchpatch {
-          excludes = [ "config.def.h" ];
-          url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/cursortheme/cursortheme.patch";
-          sha256 = "sha256-E544m6ca2lYbjYxyThr3GQEhDqh2SDjryLV/g4X8Rt4=";
-         })
-        ];
-        postPatch = ''
-        cp ${./modules/dwl/config.def.h} config.def.h
-        '';
-      });
-    })
-  ];
-
+  #####################
+  ### MISCELLANEOUS ###
+  #####################
   virtualisation.waydroid.enable = true;
-
-  fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
 
   #environment.variables = {
   #  XCURSOR_SIZE  = "22";
   #};
+
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Home Manager options (module is imported via flake.nix)
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+  boot.loader.systemd-boot.enable = true;
+  systemd.extraConfig = ''
+    DefaultTimeoutStopSec=10s
+    LogLevel=emerg
+  '';
+  boot.loader.systemd-boot.consoleMode = "max";
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "hp";
+  networking.networkmanager.enable = true;
 
   boot = {
     plymouth = {
@@ -235,20 +209,75 @@ in
 
   };
 
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        Experimental = true;
-        FastConnectable = true;
-      };
-      Policy = { AutoEnable = true; };
-    };
-  };
-
-  services.udisks2.enable = true;
-  services.flatpak.enable = true;
-
+  # Defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases)
+  # created on older NixOS versions.
   system.stateVersion = "25.05";
+
+  ################
+  ### HARDWARE ###
+  ################
+  hardware = {
+    # BLUETOOTH
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Experimental = true;
+          FastConnectable = true;
+        };
+        Policy = { AutoEnable = true; };
+      };
+    };
+    # ...
+  };
+  ############
+  ### TIME ###
+  ############
+  time.timeZone = "Pacific/Auckland";
+  services.timesyncd.enable = true;
+
+  ##################
+  ### AUTO MOUNT ###
+  ##################
+  fileSystems."/run/media/mrn1/data" = {
+    device = "/dev/disk/by-uuid/06EE19DCEE19C539";
+    fsType = "ntfs-3g";
+    options = [ "rw" "uid=1000" "gid=100" "umask=0022" ];
+  };
+  #############
+  ### USERS ###
+  #############
+  users.users.mrn1 = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [ tree ];
+  };
 }
+
+# KEPT FOR REFERENCE: IF YOU NEED TO USE IT, Move it at the top before the opening attribute set
+/*
+let
+  # Local derivation example
+  cage-xtmapper = pkgs.stdenv.mkDerivation {
+    pname = "cage-xtmapper";
+    version = "1.0";
+    src = ./pkgs/cage-xtmapper/cage-xtmapper-v0.2.0.tar;
+
+    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+    buildInputs = with pkgs; [
+      wayland libdrm libxkbcommon pixman mesa vulkan-loader systemd seatd
+      xorg.libxcb xorg.xcbutilrenderutil xorg.xcbutil libGL
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp local/bin/cage_xtmapper $out/bin/
+      cp local/bin/cage_xtmapper.sh $out/bin/
+      chmod +x $out/bin/*
+    '';
+  };
+in 
+*/
+
---
+++ hosts/hp/pkgs/cage-xtmapper/cage-xtmapper.nix
@@ -0,0 +1,20 @@
+{ pkgs,  ... }:
+  pkgs.stdenv.mkDerivation {
+    pname = "cage-xtmapper";
+    version = "1.0";
+    src = ./cage-xtmapper-v0.2.0.tar;
+
+    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+    buildInputs = with pkgs; [
+      wayland libdrm libxkbcommon pixman mesa vulkan-loader systemd seatd
+      xorg.libxcb xorg.xcbutilrenderutil xorg.xcbutil libGL
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp local/bin/cage_xtmapper $out/bin/
+      cp local/bin/cage_xtmapper.sh $out/bin/
+      chmod +x $out/bin/*
+    '';
+
+}
