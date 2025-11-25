9bedbcac73a1431a720e499e38b16f7c522a0adf
Mrn157 <gamerzzz1510@gmail.com>
2025-11-25 22:44:25 +1300

    Added nvim remember last cursor position

--- hosts/hp/modules/nvchad/nvchad.nix
+++ hosts/hp/modules/nvchad/nvchad.nix
@@ -55,6 +55,23 @@ require("nvim-tree").setup({
   },
 })
 
+-- https://nvchad.com/docs/recipes/
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
 -- read :h vim.lsp.config for changing options of lsp servers 
     '';
     extraPlugins = /* lua */ ''
