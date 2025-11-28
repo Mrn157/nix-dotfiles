57ecbfb50dc11f67cfeec8ae0915a3c57ce2b158
Mrn157 <gamerzzz1510@gmail.com>
2025-11-29 00:50:45 +1300

    Changed obs-studio to unstable

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -152,6 +152,7 @@
 
     obs-studio = {
     enable = true;
+    package = pkgs-unstable.obs-studio;
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs obs-backgroundremoval obs-pipewire-audio-capture
       obs-vaapi obs-gstreamer obs-vkcapture
