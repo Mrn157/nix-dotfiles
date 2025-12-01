dc674107de2d7f58ecbcbcd322f8d14929de4a06
Mrn157 <gamerzzz1510@gmail.com>
2025-12-01 21:43:12 +1300

    Fetch cage-xtmapper instead of sourcing it locally + Code changes

--- hosts/hp/pkgs/cage-xtmapper/cage-xtmapper.nix
+++ hosts/hp/pkgs/cage-xtmapper/cage-xtmapper.nix
@@ -1,20 +1,23 @@
 { pkgs,  ... }:
   pkgs.stdenv.mkDerivation {
-    pname = "cage-xtmapper";
-    version = "1.0";
-    src = ./cage-xtmapper-v0.2.0.tar;
+  pname = "cage-xtmapper";
+  version = "1.0";
+  # src = ./cage-xtmapper-v0.2.0.tar;
+  src = pkgs.fetchurl {
+    url = "https://github.com/Xtr126/cage-xtmapper/releases/download/v20250920/cage-xtmapper-v0.2.0.tar";
+    hash = "sha256-YixHjSsthYC52ozdJ/ZF+h6BxEKRLha/uRixJfunIgU=";
+  };
 
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = with pkgs; [
-      wayland libdrm libxkbcommon pixman mesa vulkan-loader systemd seatd
-      xorg.libxcb xorg.xcbutilrenderutil xorg.xcbutil libGL
-    ];
+  nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+  buildInputs = with pkgs; [
+    wayland libdrm libxkbcommon pixman mesa vulkan-loader systemd seatd
+    xorg.libxcb xorg.xcbutilrenderutil xorg.xcbutil libGL
+  ];
 
-    installPhase = ''
-      mkdir -p $out/bin
-      cp local/bin/cage_xtmapper $out/bin/
-      cp local/bin/cage_xtmapper.sh $out/bin/
-      chmod +x $out/bin/*
-    '';
-
+  installPhase = ''
+    mkdir -p $out/bin
+    cp local/bin/cage_xtmapper $out/bin/
+    cp local/bin/cage_xtmapper.sh $out/bin/
+    chmod +x $out/bin/*
+  '';
 }
