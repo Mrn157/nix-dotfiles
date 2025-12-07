b739158310c57a514a57c417c73007de24d8cfd8
Mrn157 <gamerzzz1510@gmail.com>
2025-12-07 15:52:36 +1300

    Removed 'nixConfig' in flake.nix

--- flake.nix
+++ flake.nix
@@ -107,9 +107,5 @@
           ];
         };
       };
-      nixConfig = {
-        extra-substituters = [ "https://mrn157.cachix.org" ];
-        extra-trusted-public-keys = [ "mrn157.cachix.org-1:A3KuzqTH/AeTFpDsu7Fql7KpZBJvFCkfNqxkL2+DZlc=" ];
-      };
     };
 }
