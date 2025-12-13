7e3ec5285ce53bd1bd9d273411e70099ed851cd4
Mrn157 <gamerzzz1510@gmail.com>
2025-12-13 19:10:23 +1300

    Added rofimoji + niri keybind for it

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -78,6 +78,7 @@
       mission-center xwayland-satellite wev wgcf wireguard-tools unrar cachix
       nix-init zed-editor cemu nixd nil python3 yad eza rofi waydroid-helper
       ninja meson plocate gnumake mpv tmux p7zip lutris neovide steam-run xorg.libSM
+      rofimoji
       # gvfs (if you want custom folder icons on nemo + trash folder)
 
     /*  Call the function which is in cage-xtmapper.nix, give it the current pkgs set as input
