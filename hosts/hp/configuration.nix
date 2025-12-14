63b19b0a32cffd85a6566350faa96b4ef47c5a1c
Mrn157 <gamerzzz1510@gmail.com>
2025-12-15 11:35:08 +1300

    Made the tray label on yambar work by using tray-tui

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -79,6 +79,7 @@
       nix-init zed-editor cemu nixd nil python3 yad eza rofi waydroid-helper
       ninja meson plocate gnumake mpv tmux p7zip lutris neovide steam-run xorg.libSM
       rofimoji
+      tray-tui
       # gvfs (if you want custom folder icons on nemo + trash folder)
 
     /*  Call the function which is in cage-xtmapper.nix, give it the current pkgs set as input
